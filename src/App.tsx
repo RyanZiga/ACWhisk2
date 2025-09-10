eb2ba8db54b5f9e1b1825d7ded26aad0bc071c8b
RyanZiga <87555605+RyanZiga@users.noreply.github.com>
2025-09-11 02:00:38 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { AnimatePresence } from 'framer-motion'
 import { AuthProvider, useAuth } from './components/AuthContext'
 import { ThemeProvider } from './components/ThemeContext'
 import { NotificationProvider } from './components/ui/notification'
